df1ada320c9e653f9a8ded66835a23137993697a
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-07-16 01:02:13 -0300

    feat: first class finished

---
+++ 01 - Objetos/aula01/public/scripts/main.js
@@ -0,0 +1,14 @@
+let car = {
+  qtdPorts: 4,
+  color: "Red"
+};
+
+console.log("CAR", car);
+console.log("TYPEOF", typeof car);
+
+let person = {
+  name: "Gabriel"
+};
+
+console.log("PERSON", person);
+
