2708ec44415905a73b002881ed8bbf1b66f75d4d
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-07-16 09:40:29 -0300

    feat: fourth class finished

---
+++ 01 - Objects/aula04/public/scripts/main.js
@@ -0,0 +1,10 @@
+let horse = {
+  paws: 4,
+  "is domesticated": false,
+  isDomesticated: false,
+}
+
+console.log("HORSE", horse);
+console.log("HORSE PAWS", horse.paws);
+console.log("IS DOMESTICATED", horse["is domesticated"]);
+console.log("IS DOMESTICATED WITH camelCase", horse.isDomesticated);
