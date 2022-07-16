685f90f037f22a28cd495a22bc5103cb794e3b27
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-07-16 10:59:06 -0300

    feat: fifth class finished

---
+++ 01 - Objects/aula05/public/scripts/main.js
@@ -0,0 +1,11 @@
+let horse = {
+  paws: 4,
+  "is domesticated": false,
+  isDomesticated: false,
+}
+
+const numberOfPaws = 'paws';
+const domesticated = "is domesticated";
+
+console.log("HORSE PAWS", horse[numberOfPaws]);
+console.log("HORSE PAWS", horse[domesticated]);
