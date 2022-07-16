78e5fe236b948b8e72d62b6fd5268d7c432857f9
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-07-16 08:53:50 -0300

    feat: second class finished

--- 01 - Objects/aula02/public/scripts/main.js
+++ 01 - Objects/aula02/public/scripts/main.js
@@ -1,1 +1,21 @@
+let car = {
+  qtdPorts: 4,
+  color: "Red"
+};
 
+console.log("QTDPorts", car.qtdPorts);
+console.log("Color", car.color);
+
+if(car.qtdPorts > 2) {
+  console.log("This car have more than 2 ports")
+}
+
+let person = {
+  name: "Gabriel"
+};
+
+console.log("PERSON", person["name"]);
+
+let personName = person["name"];
+
+console.log("PersonName", personName);
